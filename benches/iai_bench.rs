b010f9c701577228958d17a8c2168e990d659341
natkeo559 <natkeo1@gmail.com>
2023-02-09 23:50:11 -0500

    iai Benches Temporarily Removed

--- benches/iai_bench.rs
+++ benches/iai_bench.rs
@@ -1,21 +1,21 @@
-use iai;
+// use iai;
 
-use lo_shu::order_three::permute::*;
+// use lo_shu::order_three::permute::*;
 
-fn iai_simd_single_u8_arr() {
-    simd_threaded_kth_u8::iterate_perms();
-}
+// fn iai_simd_single_u8_arr() {
+//     simd_threaded_kth_u8::iterate_perms();
+// }
 
-fn iai_simd_single_u8_vec() {
-    simd_perms_u8::iterate_perms();
-}
+// fn iai_simd_single_u8_vec() {
+//     simd_perms_u8::iterate_perms();
+// }
 
-fn iai_simd_double_u8_vec() {
-    simd_double_perms_u8::iterate_perms();
-}
+// fn iai_simd_double_u8_vec() {
+//     simd_double_perms_u8::iterate_perms();
+// }
 
-iai::main!(
-    iai_simd_single_u8_arr,
-    iai_simd_single_u8_vec,
-    iai_simd_double_u8_vec
-);
+// iai::main!(
+//     iai_simd_single_u8_arr,
+//     iai_simd_single_u8_vec,
+//     iai_simd_double_u8_vec
+// );
