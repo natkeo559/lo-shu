57158db55597ae1c895867f74d7b8cf6f3e78eb6
natkeo559 <natkeo1@gmail.com>
2023-02-19 20:53:34 -0500

    Updated benchmarks

--- benches/perf_function.rs
+++ benches/perf_function.rs
@@ -1,10 +1,10 @@
-use lo_shu::{Check, Square};
+use lo_shu::{Check, Square3};
 use rayon::prelude::*;
 
 fn perm_sol() {
     let _ = (0..362879 / 2)
         .into_par_iter()
-        .filter_map(|k| Square::<u8>::kth_perm(k).check_simd_single())
+        .filter_map(|k| Square3::<u8>::kth_perm(k).check_simd_single())
         .collect::<Vec<_>>();
 }
 
