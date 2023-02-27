c3ceff976d8587b9701470a70b47ab59ac64776e
natkeo559 <natkeo1@gmail.com>
2023-02-27 18:44:12 -0500

    Added to pack, transform

--- src/core/pack.rs
+++ src/core/pack.rs
@@ -19,6 +19,30 @@ pub fn unpack_4xu4(p: u16) -> (u8, u8, u8, u8) {
     )
 }
 
+pub fn pack_8xu4(a: u8, b: u8, c: u8, d: u8, e: u8, f: u8, g: u8, h: u8) -> u32 {
+    (h as u32 & 0x0F)
+        | (g as u32 & 0x0F) << 4
+        | (f as u32 & 0x0F) << 8
+        | (e as u32 & 0x0F) << 12
+        | (d as u32 & 0x0F) << 16
+        | (c as u32 & 0x0F) << 20
+        | (b as u32 & 0x0F) << 24
+        | (a as u32 & 0x0F) << 28
+}
+
+pub fn unpack_8xu4(p: u32) -> (u8, u8, u8, u8, u8, u8, u8, u8) {
+    (
+        (p >> 28) as u8 & 0x0F,
+        (p >> 24) as u8 & 0x0F,
+        (p >> 20) as u8 & 0x0F,
+        (p >> 16) as u8 & 0x0F,
+        (p >> 12) as u8 & 0x0F,
+        (p >> 8) as u8 & 0x0F,
+        (p >> 4) as u8 & 0x0F,
+        p as u8 & 0x0F,
+    )
+}
+
 #[cfg(test)]
 mod pack_test {
     use super::*;
@@ -29,14 +53,17 @@ mod pack_test {
         let b: u8 = 15;
         let c: u8 = 15;
         let d: u8 = 15;
-        let p2: u8 = pack_2xu4(a, b);
+        let e: u8 = 15;
+        let f: u8 = 15;
+        let g: u8 = 15;
+        let h: u8 = 15;
+        
+        let p2 = pack_2xu4(a, b);
         let p4 = pack_4xu4(a, b, c, d);
-
-        // assert_eq!(31, p2);
+        let p8 = pack_8xu4(a, b, c, d, e, f, g, h);
 
         assert_eq!((a, b), unpack_2xu4(p2));
         assert_eq!((a, b, c, d), unpack_4xu4(p4));
-
-        println!("{} {}", p2, p4);
+        assert_eq!((a, b, c, d, e, f, g, h), unpack_8xu4(p8));
     }
 }
--- src/core/transform.rs
+++ src/core/transform.rs
@@ -1,7 +1,9 @@
-use crate::{Square, Permutation};
+use crate::{Permutation, Square};
 
 pub trait SquareTransforms<T: Copy + Clone, const N: usize> {
     fn perm_id(&mut self) -> Permutation<T, N>;
+    fn rotate_90(&mut self) -> Self;
+    fn reflect_x(&mut self) -> Self;
 }
 
 impl SquareTransforms<u8, 9> for Square<u8, 9> {
@@ -28,24 +30,69 @@ impl SquareTransforms<u8, 9> for Square<u8, 9> {
             result += index * factor;
             j += 1;
         }
-        Permutation { square: *self, index: result - 46234}
-        
+        Permutation {
+            square: *self,
+            index: result - 46234,
+        }
+    }
+    fn rotate_90(&mut self) -> Self {
+        Square([
+            self[6], self[3], self[0], self[7], self[4], self[1], self[8], self[5], self[2],
+        ])
+    }
+
+    fn reflect_x(&mut self) -> Self {
+        Square([
+            self[2], self[1], self[0], self[5], self[4], self[3], self[8], self[7], self[6],
+        ])
     }
 }
 
 #[cfg(test)]
 mod test_transform {
-    use crate::{OrderThree, Params, Permutation, SquareTransforms};
+    use crate::{OrderThree, Params, Permutation, Square, SquareTransforms};
 
     #[test]
     fn test_id() {
         let mut a = Permutation::<u8, 9>::first().square;
-        assert_eq!(Permutation { square: a, index: 0}, a.perm_id());
+        assert_eq!(
+            Permutation {
+                square: a,
+                index: 0
+            },
+            a.perm_id()
+        );
 
         let mut a = Permutation::<u8, 9>::kth(OrderThree::PERMUTATIONS - 1).square;
-        assert_eq!(Permutation { square: a, index: OrderThree::PERMUTATIONS - 1}, a.perm_id());
+        assert_eq!(
+            Permutation {
+                square: a,
+                index: OrderThree::PERMUTATIONS - 1
+            },
+            a.perm_id()
+        );
 
         let mut a = Permutation::<u8, 9>::kth(499).square;
-        assert_eq!(Permutation { square: a, index: 499}, a.perm_id());
+        assert_eq!(
+            Permutation {
+                square: a,
+                index: 499
+            },
+            a.perm_id()
+        );
+    }
+
+    #[test]
+    fn test_rotate_90() {
+        let mut a = Permutation::<u8, 9>::first().square;
+        let b = a.rotate_90().rotate_90();
+        assert_eq!(Square([9, 8, 7, 6, 5, 4, 3, 2, 1]), b);
+    }
+
+    #[test]
+    fn test_reflect_x() {
+        let mut a = Permutation::<u8, 9>::first().square;
+        let b = a.reflect_x();
+        assert_eq!(Square([3, 2, 1, 6, 5, 4, 9, 8, 7]), b);
     }
 }
