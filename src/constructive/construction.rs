db58167c29fc029ffd106e68b58254e9190b441d
natkeo559 <natkeo1@gmail.com>
2023-07-06 23:10:50 -0400

    fixed bugs, changed names, added functions

--- src/constructive/construction.rs
+++ src/constructive/construction.rs
@@ -12,23 +12,39 @@ impl<P: Params + Copy> Construction<P>
 where
     [(); P::ELEMENTS]:,
 {
-    pub fn new() -> Self {
+    pub fn zeros() -> Self {
         Construction {
-            square: Square::<P>::from_array([0; P::ELEMENTS]),
+            square: Square([0; P::ELEMENTS])
         }
     }
 
-    pub fn siamese(&self, seed_idx: usize) -> Self {
+    pub fn identity() -> Self {
+        let mut arr: [u8; P::ELEMENTS] = [0; P::ELEMENTS];
+        for (elem, val) in arr.iter_mut().zip(1..=P::ELEMENTS as u8) {
+            *elem = val;
+        }
+
+        Self {
+            square: Square(arr)
+        }
+
+    }
+
+    pub fn siamese(seed_idx: usize) -> Self {
         assert!(P::ORDER % 2 != 0, "Order Must Be Odd!");
 
-        let mut c = *self;
+        let mut c = Self::zeros();
 
-        let mut next_pos = (seed_idx / 3, seed_idx % 3);
+        let mut next_pos = (seed_idx / P::ORDER, seed_idx % P::ORDER);
+
         for i in 1..=P::ELEMENTS as u8 {
+
             c.square[(next_pos.0 * P::ORDER) + next_pos.1] = i;
-            next_pos = ((next_pos.0 + 2) % P::ORDER, (next_pos.1 + 1) % P::ORDER);
-            if c.square[(next_pos.0 * P::ORDER) + next_pos.1] != 0 {
-                next_pos = ((next_pos.0 + 2) % P::ORDER, (next_pos.1 + 2) % P::ORDER);
+
+            next_pos = ((next_pos.0 + (P::ORDER - 1)) % P::ORDER, (next_pos.1 + 1) % P::ORDER);
+
+            if c.square[next_pos.0 * P::ORDER + next_pos.1] != 0 {
+                next_pos = ((next_pos.0 + 2) % P::ORDER, (next_pos.1 + (P::ORDER - 1)) % P::ORDER);
             }
         }
 
@@ -41,19 +57,19 @@ where
     [(); P::ELEMENTS]:,
 {
     fn default() -> Self {
-        Self::new()
+        Self::zeros()
     }
 }
 
 #[cfg(test)]
 mod test_construction {
-    use crate::{CheckVector, OrderThree};
+    use crate::{CheckVector, OrderThree, OrderFive};
 
     use super::*;
 
     #[test]
-    fn test_new() {
-        let a = Construction::<OrderThree>::new();
+    fn test_new_zeros() {
+        let a = Construction::<OrderThree>::zeros();
         let b = Construction {
             square: Square([0, 0, 0, 0, 0, 0, 0, 0, 0]),
         };
@@ -61,18 +77,42 @@ mod test_construction {
     }
 
     #[test]
-    fn test_siamese() {
-        let a = Construction::<OrderThree>::new();
-        let b = a.siamese(1);
-        println!("{}", b.square)
+    fn test_new_identity() {
+        let a = Construction::<OrderThree>::identity();
+        let b = Construction {
+            square: Square([1, 2, 3, 4, 5, 6, 7, 8, 9]),
+        };
+        assert_eq!(a, b);
     }
 
     #[test]
+    fn test_siamese() {
+        let a = Construction::<OrderThree>::siamese(1);
+        let b = Construction {
+            square: Square([8, 1, 6, 3, 5, 7, 4, 9, 2]),
+        };
+
+        assert_eq!(a, b);
+        assert!(a.check_v().is_some())
+    }
+
+    #[test]
+    #[ignore = "debugging"]
     fn test_valid_siamese() {
         for i in 0..OrderThree::ELEMENTS {
-            if let Some(a) = Construction::<OrderThree>::new().siamese(i).check_v() {
+            if let Some(a) = Construction::<OrderThree>::siamese(i).check_v() {
                 println!("{}", a.square)
             };
         }
+
+        let mut sols = vec![];
+        for i in 0..OrderFive::ELEMENTS {
+            let a = Construction::<OrderFive>::siamese(i);
+            if a.check_n_v().is_some() {
+                sols.push(a);
+                println!("{}\n", a.square)
+            };
+        }
+        println!("{}", sols.len())
     }
 }
