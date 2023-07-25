fbc31cd8ec1438ddcbe561235daa16e3549776fc
natkeo559 <natkeo1@gmail.com>
2023-07-24 20:48:32 -0400

    Refactored Group Action Algorithm

--- examples/group_action_analysis.rs
+++ examples/group_action_analysis.rs
@@ -5,11 +5,43 @@ use lo_shu::{CheckVector, OrderFour, Permutation};
 use rayon::prelude::{IntoParallelIterator, ParallelBridge, ParallelIterator};
 
 use itertools::Itertools;
-use std::collections::{BTreeSet, HashSet};
+use std::collections::BTreeSet;
 use std::fs::read_to_string;
 use std::fs::File;
 use std::io::Write;
 use std::time::Instant;
+
+fn unique_squares(origin: &BTreeSet<usize>) -> BTreeSet<usize> {
+    let mut unique_set = BTreeSet::new();
+    for s in origin.iter() {
+        if unique_set
+            .intersection(&Permutation::<OrderFour>::kth(*s).generate_d_indexes())
+            .map(|i| *i)
+            .collect::<BTreeSet<usize>>()
+            .is_empty()
+        {
+            unique_set.insert(*s);
+        }
+    }
+    unique_set
+}
+
+fn compute_group_actions(group: &BTreeSet<usize>) -> BTreeSet<usize> {
+    group
+        .iter()
+        .cartesian_product(group.iter())
+        .par_bridge()
+        .map(|(&i, &j)| {
+            let a = Permutation::<OrderFour>::kth(i);
+            let c = Permutation::<OrderFour>::kth(j);
+
+            // Factored action "b" from a * b = c
+            // b = (c.inv * a).inv
+            // Collect these into a set.
+            (a.inv() * c).index
+        })
+        .collect::<BTreeSet<usize>>()
+}
 
 fn main() {
     let start = Instant::now();
@@ -30,10 +62,10 @@ fn main() {
     //
     // As more elements are taken, the computation time increases exponentially. To generate all magic squares
     // from Part.txt, take at least 445 elements. For Census.txt, take at least 447 elements.
-    let mut magic_squares = read_to_string("examples/collected/orderfour/Census.txt")
+    let magic_squares = read_to_string("examples/collected/orderfour/Reduced.txt")
         .expect("Could not find input file")
         .lines()
-        .take(1100)
+        .take(50)
         .map(|line| line.trim().parse::<usize>().unwrap())
         .collect::<BTreeSet<usize>>();
 
@@ -44,51 +76,17 @@ fn main() {
     println!("Input Size: {}", magic_squares.len());
 
     // Filter the set of magic squares from file to contain only unique elements up to rotations and reflections.
-    let mut unique_set = BTreeSet::new();
-    for s in magic_squares.iter() {
-        if unique_set
-            .intersection(&Permutation::<OrderFour>::kth(*s).generate_d_indexes())
-            .map(|i| *i)
-            .collect::<BTreeSet<usize>>()
-            .is_empty()
-        {
-            unique_set.insert(*s);
-        }
-    }
+    let mut unique_set = unique_squares(&magic_squares);
 
     println!("(Unique): {}", unique_set.len());
     println!("Collecting Actions...");
 
     // Collect Actions
     // Actions: {b | a * b = c for all a,c in M of magic squares}
-    let actions = magic_squares
-        .iter()
-        .cartesian_product(magic_squares.iter())
-        .par_bridge()
-        .map(|(&i, &j)| {
-            let a = Permutation::<OrderFour>::kth(i);
-            let c = Permutation::<OrderFour>::kth(j);
-
-            // Factored action "b" from a * b = c
-            // b = (c.inv * a).inv
-            // Collect these into a set.
-            (c.inv() * a).inv().index
-        })
-        .collect::<BTreeSet<usize>>();
+    let actions = compute_group_actions(&unique_set);
 
     // Filter the set of actions to contain only unique elements up to rotations and reflections.
-    let mut unique_actions = BTreeSet::new();
-    actions.iter().for_each(|&a| {
-        let action = Permutation::<OrderFour>::kth(a);
-        if unique_actions
-            .intersection(&action.generate_d_indexes())
-            .cloned()
-            .collect::<HashSet<usize>>()
-            .is_empty()
-        {
-            unique_actions.insert(a);
-        }
-    });
+    let unique_actions: BTreeSet<usize> = unique_squares(&actions);
 
     println!("Actions: {}", unique_actions.len());
     println!("Extending...");
@@ -107,7 +105,7 @@ fn main() {
     // to any other magic square. For example, if a magic square `a` and action `b` resulted in a magic square
     // then apply `b` to all other magic squares. If `b` fails to produce a magic square, reject it - insert it
     // into the rejection set. Think of this as "failure by counter-example".
-    let zipped: (BTreeSet<_>, BTreeSet<_>) = magic_squares
+    let zipped: (BTreeSet<_>, BTreeSet<_>) = unique_set
         .iter()
         .cartesian_product(unique_actions.iter())
         .par_bridge()
@@ -117,20 +115,18 @@ fn main() {
             let square = Permutation::<OrderFour>::kth(s);
             let action = Permutation::<OrderFour>::kth(a);
 
-            magic.extend(square.generate_d_indexes());
             magic.extend(
                 square
                     .generate_d()
                     .into_iter()
                     .filter_map(|s| {
                         if let Some(m) = (s * action).check_v() {
-                            Some(m.generate_d_indexes())
+                            Some(m.index)
                         } else {
                             reject.insert(action.index);
                             None
                         }
                     })
-                    .flatten()
                     .collect::<BTreeSet<usize>>(),
             );
             (magic, reject)
@@ -138,10 +134,11 @@ fn main() {
         .unzip();
 
     // Unpack the zipped data into its respective set.
-    magic_squares.extend(zipped.0.into_par_iter().flatten().collect::<BTreeSet<_>>());
+    unique_set.extend(zipped.0.into_par_iter().flatten().collect::<BTreeSet<_>>());
     let reject = zipped.1.into_par_iter().flatten().collect::<BTreeSet<_>>();
-
-    println!("|M| = {}", magic_squares.len());
+    
+    let unique_set = unique_squares(&unique_set);
+    println!("|M| = {}", unique_set.len());
     println!("|A| = {}", unique_actions.len());
 
     // Set difference between unique actions and the rejection set, leaving only actions that DO result in a
@@ -158,19 +155,19 @@ fn main() {
     println!("Completed in {} seconds", elapsed.as_secs());
 
     // Write files containing a census of order 4 magic squares (not unique) and actions in G (unique)
-    if magic_squares.len() == 7040 {
-        let mut outfile = File::create("examples/collected/orderfour/Census.txt").unwrap();
-        for i in magic_squares.iter() {
+    if unique_set.len() == 880 {
+        let mut outfile = File::create("examples/collected/orderfour/UniqueCensus.txt").unwrap();
+        for i in unique_set.iter() {
             write!(outfile, "{}\n", i).unwrap();
         }
     }
 
-    // if !g.is_empty() {
-    //     let mut outfile = File::create("examples/collected/orderfour/G.txt").unwrap();
-    //     for i in g.iter() {
-    //         write!(outfile, "{}\n", i).unwrap();
-    //     }
-    // }
+    if !g.is_empty() {
+        let mut outfile = File::create("examples/collected/orderfour/G.txt").unwrap();
+        for i in g.iter() {
+            write!(outfile, "{}\n", i).unwrap();
+        }
+    }
 }
 
 #[cfg(test)]
@@ -186,8 +183,26 @@ mod debugging {
             .map(|line| line.trim().parse::<usize>().unwrap())
             .collect::<BTreeSet<usize>>();
 
-        for i in g.into_iter().map(|i| Permutation::<OrderFour>::kth(i)) {
-            println!("{}", i.cyclic_notation().order())
+        let s = read_to_string("examples/collected/orderfour/Census.txt")
+            .expect("Could not find input file")
+            .lines()
+            .map(|line| line.trim().parse::<usize>().unwrap())
+            .collect::<BTreeSet<usize>>();
+
+        for j in g.into_iter().map(|j| Permutation::<OrderFour>::kth(j)){
+            println!("Action: \n{}", j);
+            println!("Order: {}", j.cyclic_notation().order());
+            let mut passed = 0;
+            let mut failed = 0;
+            for i in s.iter().map(|i| Permutation::<OrderFour>::kth(*i)) {
+                // assert!((j * i).check_v().is_some());
+                if (i * j).check_v().is_some() {
+                    passed += 1;
+                } else {
+                    failed += 1;
+                }
+            }
+            println!("Passed: {}\nFailed: {}\n", passed, failed);
         }
     }
 }
